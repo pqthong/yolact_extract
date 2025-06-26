a885cc774e3814e1a6609e594c12c7bff3bea233
root <root@DESKTOP-IPDB6V8.>
2025-06-26 11:58:06 +0700

    a

--- eval.py
+++ eval.py
@@ -27,14 +27,7 @@ def parse_args(argv=None):
                         help='Trained state_dict file path to open. If "interrupt", this will open the interrupt file.')
     parser.add_argument('--image', default=None, type=str,
                         help='A path to an image to use for display.')
-    parser.add_argument('--detect', default=False, dest='detect', action='store_true',
-                        help='Don\'t evauluate the mask branch at all and only do object detection. This only works for --display and --benchmark.')
 
-    parser.add_argument('--config', default=None,
-                        help='The config object to use.')
-    parser.set_defaults(no_bar=False, display=False, resume=False, output_coco_json=False, output_web_json=False, shuffle=False,
-                        benchmark=False, no_sort=False, no_hash=False, mask_proto_debug=False, crop=True, detect=False, display_fps=False,
-                        emulate_playback=False)
 
     global args
     args = parser.parse_args(argv)
@@ -43,11 +36,10 @@ def parse_args(argv=None):
 if __name__ == '__main__':
     parse_args()
 
-    if args.config is None:
-        model_path = SavePath.from_str(args.trained_model)
-        args.config = model_path.model_name + '_config'
-        print('Config not specified. Parsed %s from the file name.\n' % args.config)
-        set_cfg(args.config)
+    model_path = SavePath.from_str(args.trained_model)
+    args.config = model_path.model_name + '_config'
+    print('Config not specified. Parsed %s from the file name.\n' % args.config)
+    set_cfg(args.config)
 
     print('Loading model...', end='')
     net = Yolact(model_path=args.trained_model)
